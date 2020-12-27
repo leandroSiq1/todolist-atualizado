09df8743f48b8ef534c2290dde5fd5330388f39a
LeandroSiq1 <le_kts12@hotmail.com>
2020-12-27 19:33:43 -0300

    update; removi bug, e init section edit

--- scripts/index.js
+++ scripts/index.js
@@ -6,7 +6,6 @@ const input = document.getElementById("tarefa");
 function createTask() {
 
     let value = input.value;
-
     let task = document.createElement("div");
     let icons = document.createElement("div");
     let p = document.createElement("p");
@@ -15,27 +14,60 @@ function createTask() {
 
     if(value != "") {
         display.appendChild(task);
-    task.classList.add("task");
-    task.id = "task";
-    task.appendChild(p);
-    task.appendChild(icons);
+        task.classList.add("task");
+        task.id = "task";
+        task.appendChild(p);
+        task.appendChild(icons);
+
+        icons.classList = "icons";
+        iconEdite.classList = iconEdit;
+        iconDel.classList = iconDelete;
+
+        icons.appendChild(iconEdite);
+        icons.appendChild(iconDel);
     
-    icons.onclick = function remove() {
-        this.parentElement.remove();
-    }
+        icons.childNodes[1].addEventListener("click", () => {
+            icons.childNodes[1].parentNode.parentNode.remove();
+        });
 
-    iconEdite.classList = iconEdit;
-    iconDel.classList = iconDelete;
+        icons.childNodes[0].addEventListener("click", () => {
+            edit(icons.childNodes[0].parentNode.parentNode);
+            confirmEdit(icons.childNodes[0].parentNode.parentNode);
+        });
 
-    icons.appendChild(iconEdite);
-    icons.appendChild(iconDel);
-
-    p.innerText = value;
-    
-    input.value = "";      
+        p.innerText = value;
+        
+        input.value = "";      
     }
 }
 
 function remove() {
     this.remove();
+}
+
+function edit() {
+    let display = document.getElementById("display");
+    let boxInput = document.getElementById("inputs");
+    let edit = document.getElementById("edit");
+
+    display.style.display = "none";
+    boxInput.style.display = "none";
+    edit.style.display = "flex";
+
+}
+
+function confirmEdit(element) {
+    let confirm = document.getElementById("confirm");
+    confirm.style.display = "flex";
+
+    let newTask = document.getElementById("editInput");
+    let value = newTask.value;
+
+    console.log(value)  
+
+
+
+
+    // element.childNodes[0]
+
 }
